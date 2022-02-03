1c4b6ad5bdcc3d59b8bd34d701ee2bf4a6867dcd
twaldin <94498628+twaldin@users.noreply.github.com>
2022-02-03 00:35:58 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+function guess_word(){  
+  document.getElementById('answer').style.backgroundColor='white'
+  document.getElementById('spelling').innerHTML = ''
+  let x = document.getElementById("guess").value;
+  var dictionary = new Typo("en_GB", false, false, { dictionaryPath: "dictionaries" });
+  var is_spelled_correctly = dictionary.check(x);
+  if (is_spelled_correctly){
+    if(x.startsWith('s')){
+      window.setTimeout(correct, 400)
+    }else{window.setTimeout(wrong, 400)}
+    
+  }else{spelt_wrong()}
+}
+
+function correct(){
+  document.getElementById('answer').style.backgroundColor='green'
+}
+
+function wrong(){
+  document.getElementById('answer').style.backgroundColor='red'
+}
+
+function spelt_wrong(){
+  document.getElementById('spelling').innerHTML = 'invalid word'
+}
+
+function spelt_right(){
+  document.getElementById('spelling').innerHTML = 'valid word'
+}
