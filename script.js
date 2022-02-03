efa206807f2af4623f74e45720237bbcdb7860fb
twaldin <94498628+twaldin@users.noreply.github.com>
2022-02-03 14:49:03 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function guess_word(){  
   document.getElementById('answer').style.backgroundColor='white'
   document.getElementById('spelling').innerHTML = ''
-  let x = document.getElementById("guess").value;
+  let x = document.getElementById("guess").value.toLowerCase();
   var dictionary = new Typo("en_GB", false, false, { dictionaryPath: "dictionaries" });
   var is_spelled_correctly = dictionary.check(x);
+  
   if (is_spelled_correctly){
     document.getElementById('spelling').innerHTML = ''
     window.setTimeout(spelt_right, 400)
