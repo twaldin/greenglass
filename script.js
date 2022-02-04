ec40c461cc544e924d378a4abd4571221a474e63
twaldin <94498628+twaldin@users.noreply.github.com>
2022-02-03 22:33:00 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,18 +5,32 @@ function guess_word(){
   var dictionary = new Typo("en_GB", false, false, { dictionaryPath: "dictionaries" });
   var is_spelled_correctly = dictionary.check(x);
   
+  
+
   if (is_spelled_correctly){
+
     document.getElementById('spelling').innerHTML = ''
     window.setTimeout(spelt_right, 400)
+
     if(x.startsWith('s')){
-      window.setTimeout(correct, 400)
+      window.setTimeout(correct(x), 400)
     }else{window.setTimeout(wrong, 400)}
     
   }else{spelt_wrong()}
 }
 
-function correct(){
+function correct(guess){
+  let width = guess.length
   document.getElementById('answer').style.backgroundColor='green'
+  draw_line(width)
+}
+  
+function draw_line(len){
+  let new_len = parseInt(len)
+  let draw_width = new_len * 27.5
+  document.getElementById('answer').innerHTML = String(draw_width)
+
+  document.getElementById('a1c').style.width = String(draw_width) + 'px'
 }
 
 function wrong(){
